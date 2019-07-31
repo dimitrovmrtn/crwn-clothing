271497be5fe0c31aa3f28969c39d7ac80b34dbb8
Martin Dimitrov <dimitrovmrtn@gmail.com>
2019-07-31 15:52:58 +0300

    updating form input and buttons and styling them

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomePage from './components/pages/homepage.component'
 import ShopPage from './components/pages/shop/shop.component'
 import {Switch, Route} from 'react-router-dom'
 import Header from './components/header/header.component'
+import SingInAndSignUpPage from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SingInAndSignUpPage} />
       </Switch>
 
     </div>
