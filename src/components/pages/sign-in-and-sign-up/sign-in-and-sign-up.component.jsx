06bc96555b3c3f949b8ed74d1a7b1160e6565f12
Martin Dimitrov <dimitrovmrtn@gmail.com>
2019-08-04 17:22:34 +0300

    implementing firebase utils and storing user info to firebase database

--- src/components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import SignIn from '../../sign-in/sign-in.component'
+import SignUp from '../../sign-up/sign-up.component'
 
 import './sign-in-and-sign-up.styles.scss'
 
 const SingInAndSignUpPage = () => (
-  <div className="sign-in-and-sign-up"><SignIn /></div>
+  <div className="sign-in-and-sign-up"><SignIn /><SignUp /></div>
 )
 
 export default SingInAndSignUpPage;
