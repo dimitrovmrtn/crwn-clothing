7d6f19b9f14774891e87423d400c52c0ac819fe8
Martin Dimitrov <dimitrovmrtn@gmail.com>
2019-08-08 15:32:02 +0300

    adding cartIcon and cartDropdown components, implementing cartReducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,11 +3,13 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.styles.scss'
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
   <div className="header">
     <Link to="/" className="logo-container">
       <Logo className="logo" />
@@ -17,16 +19,19 @@ const Header = ({currentUser}) => (
       <Link to="/contact" className="option">CONTACT</Link>
       {
         currentUser ? 
-        <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div> 
+        (<div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>)
         : 
-        <Link className="option" to="/signin">SIGN IN</Link>
+        (<Link className="option" to="/signin">SIGN IN</Link>)
       }
+      <CartIcon />
     </div>
+    { hidden ? null : <CartDropdown /> }
   </div>
 )
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header)
